a4149822afac3e8f34d1cd3bc3aeab728ce03533
esellers25 <erin.e.sellers@gmail.com>
2021-03-19 15:50:03 -0400

    more progress

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
     evt.preventDefault()
   })
 // your code here
-
-
 });
+
+let toDo = document.querySelector("form#create-task-form")
+let list = document.querySelector("#tasks")
+
+toDo.addEventListener("submit", function(){
+  let tasks = document.createElement("li")
+  list.innerText = ""
+  list.append(tasks)
+})
+
+
+
