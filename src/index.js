90a03e33bc38775bf78163c3c4cde5576f42169a
Jede <brahmajede@gmail.com>
2021-03-19 15:13:33 -0400

    partial submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let TaskLister = document.querySelector("form#create-task-form")
+  TaskLister.addEventListener("submit", function(evt){
+    evt.preventDefault()
+  })
+// your code here
+
+
 });
